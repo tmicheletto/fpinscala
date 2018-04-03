645ca3ae3ae94b14e731ea5d8f7c43e5d6b54a1d
Tim Micheletto <tmicheletto@seek.com.au>
2018-04-03 17:01:32 +1000

    Save

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -155,20 +155,20 @@ object PolymorphicFunctions {
   // that they only have one implementation! Here's an example:
 
   def partial1[A,B,C](a: A, f: (A,B) => C): B => C =
-    a => b => f(a, b)
+    (b: B) => f(a, b)
 
   // Exercise 3: Implement `curry`.
 
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+    (a: A) => (b: B) => f(a, b)
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+    (a: A, b: B) => f(a)(b)
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -183,12 +183,10 @@ object PolymorphicFunctions {
   // Exercise 5: Implement `compose`
 
   def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+    a => f(g(a))
 
   def main(args: Array[String]): Unit = {
 //    val arr = Array(1, 2, 3, 4, 5, 6, 7, 8)
 //    println(s"Is sorted for ${arr.deep.toString} is ${isSorted(arr, (a: Int, b: Int) => a > b)}")
-
-    val adder
   }
 }
