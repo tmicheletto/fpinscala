6a8775947a0696b8c7cd1a9786856bf3e4f8f023
Tim Micheletto <tmicheletto@seek.com.au>
2018-05-15 21:53:47 +1000

    Save

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,20 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+  def fold[A,B](t: Tree[A])(z: A => B)(b: (B,B) => B): B = t match {
+    case Leaf(v) => z(v)
+    case Branch(l,r) => b((fold(l)(z)(b)),(fold(r)(z)(b)))
+  }
 
+  def size[A](t: Tree[A]): Int =
+    fold(t)(_ => 1)(1 + _ + _)
 
+  def maximum(t: Tree[Int]): Int =
+    fold(t)(a => a)((l,r) => l max r)
 
+  def depth[A](t: Tree[A]): Int =
+    fold(t)(_ => 0)((l,r) => 1 + (l max r))
+
+  def map[A,B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_,_))
 }
--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -4,21 +4,44 @@ package fpinscala.errorhandling
 import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
 sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = ???
+ def map[B](f: A => B): Either[E, B] = this match {
+     case Right(b) => Right(f(b))
+     case Left(e) => Left(e)
+   }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+ def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+     case Right(a) => f(a)
+     case Left(ee) => Left(ee)
+   }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+ def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+   case Right(a) => Right(a)
+   case Left(_) => b
+ }
+
+ def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+//   this flatMap (a => map(b1 => f(a,b1)))
+  for {
+    a <- this
+    b1 <- b
+  } yield f(a,b1)
 }
 case class Left[+E](get: E) extends Either[E,Nothing]
 case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    es match {
+      case Nil => Right(Nil)
+      case h :: t => f(h).map2(traverse(t)(f))(_ :: _)
+    }
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
+  def traverse1[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    es.foldRight[Either[E,List[B]]](Right(Nil))((a,b) => f(a).map2(b)(_ :: _))
+
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
+    traverse1(es)(A => A)
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
     if (xs.isEmpty) 
